958c8496cb5c2ac4e856786ed99b11095d901c9e
Flavio Luis Gonçalves <flavio.goncalves@sigma.com.br>
2018-05-23 17:52:23 -0300

    commit aula 46

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,11 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component'
+import { RestaurantsService } from './restaurants/restaurants.service';
+import { RestaurantDetailComponent } from './restaurants/restaurant-detail/restaurant-detail.component';
+import { MenuComponent } from './restaurants/restaurant-detail/menu/menu.component'
+import { MenuItemComponent } from './restaurants/restaurant-detail/menu-item/menu-item.component';
+import { ShoppingCartComponent } from './restaurants/restaurant-detail/shopping-cart/shopping-cart.component'
 
 
 @NgModule({
@@ -20,14 +25,18 @@ import { RestaurantComponent } from './restaurants/restaurant/restaurant.compone
 		HomeComponent,
 		AboutComponent,
 		RestaurantsComponent,
-		RestaurantComponent
+		RestaurantComponent,
+		RestaurantDetailComponent,
+		MenuComponent,
+		ShoppingCartComponent,
+		MenuItemComponent
 	],
 	imports: [
 		BrowserModule,
 		HttpModule,
 		RouterModule.forRoot(ROUTES)
 	],
-	providers: [],
+	providers: [RestaurantsService],
 	bootstrap: [AppComponent]
 })
 export class AppModule { }
