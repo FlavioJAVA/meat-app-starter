68d268844350a946207e7f92d0600d426b1dfabf
Flavio Luis Gonçalves <flavio.goncalves@sigma.com.br>
2018-05-30 17:08:48 -0300

    commit aula 76

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
-import { AboutComponent } from "./about/about.component";
 import { RestaurantsComponent } from "./restaurants/restaurants.component";
 import { RestaurantDetailComponent } from "./restaurants/restaurant-detail/restaurant-detail.component";
 import { MenuComponent } from "./restaurants/restaurant-detail/menu/menu.component";
@@ -11,7 +10,6 @@ import { OrderSummaryComponent } from "./order-summary/order-summary.component";
 
 export const ROUTES: Routes = [
 	{ path: '', component: HomeComponent },
-	{ path: 'about', component: AboutComponent },
 	{ path: 'restaurants', component: RestaurantsComponent },
 	{
 		path: 'restaurants/:id', component: RestaurantDetailComponent,
@@ -21,8 +19,8 @@ export const ROUTES: Routes = [
 			{ path: 'reviews', component: ReviewsComponent }
 		]
 	},
+	{ path: 'about', loadChildren: './about/about.module#AboutModule' },
 	{ path: 'order', component: OrderComponent },
-	{ path: 'about', component: AboutComponent },
 	{ path: 'order-summary', component: OrderSummaryComponent }
 
 ]
